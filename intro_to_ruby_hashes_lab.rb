d0563264e8bcfbd308581774198c9f719bbdd9f6
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-20 20:20:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] += 1
+  k = key
+  if hash[k]
+    hash[k] += 1
   else
-    hash[key] = 1
+    hash[k] = 1
   end
 end
